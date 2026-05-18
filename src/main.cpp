07cab04715aee9d0bd09cce89003a36eb16e8679
keyframe41 <keyframe41@gmail.com>
2026-05-18 22:12:21 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+int main() {
+	sf::RenderWindow window( sf::VideoMode({200, 200}) , "SFML works!" );
 	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	shape.setFillColor(sf::Color::Green);
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) window.close();
 		}
-
 		window.clear();
-		window.draw( shape );
+		window.draw(shape);
 		window.display();
 	}
 }
